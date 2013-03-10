5c6ff13e5047245a359534cb14702f6210610a15
Antony Tran <antonyt@gmail.com>
2013-03-10 14:17:01 +0900

    Add basic pager indicator.

--- src/com/tfltravelalerts/MainActivity.java
+++ src/com/tfltravelalerts/MainActivity.java
@@ -4,22 +4,23 @@ package com.tfltravelalerts;
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.app.Fragment;
 
-import com.tfltravelalerts.alerts.LineStatusAlertsFragment;
-import com.tfltravelalerts.statusviewer.LineStatusViewerListFragment;
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+
+import com.tfltravelalerts.alerts.LineStatusAlertsFragment;
+import com.tfltravelalerts.statusviewer.LineStatusViewerListFragment;
+import com.viewpagerindicator.PageIndicator;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // TODO: fix up
-        ViewPager viewPager = new ViewPager(this);
-        viewPager.setId(1);
+        setContentView(R.layout.main_activity);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        
+        // TODO: fixup
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
 
             @Override
@@ -37,11 +38,22 @@ public class MainActivity extends Activity {
                     default:
                         return null;
                 }
+            }
 
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return "Line Status";
+                    case 1:
+                        return "Alerts";
+                    default:
+                        return super.getPageTitle(position);
+                }
             }
         });
-        
-        setContentView(viewPager);
 
+        PageIndicator indicator = (PageIndicator) findViewById(R.id.view_pager_indicator);
+        indicator.setViewPager(viewPager);
     }
 }
