c24b216d9510bd1be039ac5ee866643aa6b5cdf3
Pedro Loureiro <github@pedroloureiro.net>
2013-04-06 01:12:21 +0100

    improvements to the edit/create alert screen

--- src/com/tfltravelalerts/alerts/EditAlertFragment.java
+++ src/com/tfltravelalerts/alerts/EditAlertFragment.java
@@ -1,14 +1,17 @@
 
 package com.tfltravelalerts.alerts;
 
+
 import org.holoeverywhere.LayoutInflater;
 import org.holoeverywhere.widget.Button;
 import org.holoeverywhere.widget.EditText;
+import org.holoeverywhere.widget.TextView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 
 import com.actionbarsherlock.view.Menu;
@@ -16,17 +19,20 @@ import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.tfltravelalerts.R;
 import com.tfltravelalerts.alerts.events.AddOrUpdateAlertRequest;
+import com.tfltravelalerts.alerts.events.AlertTimeSelected;
 import com.tfltravelalerts.alerts.events.AlertsUpdatedEvent;
 import com.tfltravelalerts.alerts.events.DeleteAlertRequest;
 import com.tfltravelalerts.common.eventbus.EventBusFragment;
 import com.tfltravelalerts.model.LineStatusAlert;
 import com.tfltravelalerts.model.Time;
 
+import de.greenrobot.event.EventBus;
+
 public class EditAlertFragment extends EventBusFragment {
 
     public static final String ALERT_ID_KEY = "alertId";
 
-	private static final String LOG_TAG = "EditAlertFragment";
+    private static final String LOG_TAG = "EditAlertFragment";
 
     private int mAlertId;
     private LineStatusAlert mAlert;
@@ -35,10 +41,10 @@ public class EditAlertFragment extends EventBusFragment {
     private EditText mAlertTitle;
     private DaySelectorView mDaySelectorView;
     private LineSelectorView mLineSelectorView;
-    private EditText mTimeInputField;
+    private TextView mTimeInputField;
     private Button mCancelButton;
     private Button mSaveButton;
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,10 +58,10 @@ public class EditAlertFragment extends EventBusFragment {
         inflater.inflate(R.menu.alerts_add_edit_menu, menu);
         menu.findItem(R.id.delete).setVisible(mAlertId != -1);
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.delete) {
+        if (item.getItemId() == R.id.delete) {
             deleteAlert();
             finishActivity();
         }
@@ -75,11 +81,17 @@ public class EditAlertFragment extends EventBusFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         inflateRootView(inflater, container);
         findViews();
-        setupButtons();
+        setupViews();
 
         return mRoot;
     }
 
+    public void onEventMainThread(AlertTimeSelected newTime) {
+        mTimeInputField.setText(newTime.getData().toString());
+        //avoid getting this next time we return here
+        EventBus.getDefault().removeStickyEvent(newTime);
+    }
+    
     private void inflateRootView(LayoutInflater inflater, ViewGroup container) {
         mRoot = inflater.inflate(R.layout.edit_alert_fragment, container, false);
     }
@@ -91,7 +103,7 @@ public class EditAlertFragment extends EventBusFragment {
 
         mDaySelectorView = (DaySelectorView) mRoot.findViewById(R.id.day_selector_view);
         mLineSelectorView = (LineSelectorView) mRoot.findViewById(R.id.line_selector_view);
-        mTimeInputField = (EditText) mRoot.findViewById(R.id.time_input);
+        mTimeInputField = (TextView) mRoot.findViewById(R.id.time_input);
     }
 
     private void updateDays() {
@@ -102,11 +114,11 @@ public class EditAlertFragment extends EventBusFragment {
         mLineSelectorView.setSelectedLines(mAlert.getLines());
     }
 
-    private void updateTimes() {
+    private void updateTime() {
         mTimeInputField.setText(mAlert.getTime().toString());
     }
 
-    private void setupButtons() {
+    private void setupViews() {
         mCancelButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -121,18 +133,39 @@ public class EditAlertFragment extends EventBusFragment {
                 finishActivity();
             }
         });
+
+        mTimeInputField.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "timeInputField.onClick");
+                showTimePickerDialog();
+            }
+        });
+        
+        mTimeInputField.setOnFocusChangeListener(new OnFocusChangeListener() {
+            
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    Log.d(LOG_TAG, "timeInputField.onFocusChange focus=true");
+                    //v.
+                    showTimePickerDialog();
+                }
+            }
+        });
     }
 
     private Time parseTime() {
         String input = mTimeInputField.getText().toString();
-            String[] parts = input.split(":");
-            try {
-                int hour = Integer.parseInt(parts[0]);
-                int minute = Integer.parseInt(parts[1]);
-                return new Time(hour, minute);
-            } catch (Exception e) {
-            	Log.w(LOG_TAG, "parseTime: failed to parse", e);
-            }
+        String[] parts = input.split(":");
+        try {
+            int hour = Integer.parseInt(parts[0]);
+            int minute = Integer.parseInt(parts[1]);
+            return new Time(hour, minute);
+        } catch (Exception e) {
+            Log.w(LOG_TAG, "parseTime: failed to parse "+input, e);
+        }
         return null;
     }
 
@@ -149,7 +182,7 @@ public class EditAlertFragment extends EventBusFragment {
         AddOrUpdateAlertRequest request = new AddOrUpdateAlertRequest(alert);
         getEventBus().post(request);
     }
-    
+
     private void deleteAlert() {
         DeleteAlertRequest request = new DeleteAlertRequest(mAlert);
         getEventBus().post(request);
@@ -170,7 +203,18 @@ public class EditAlertFragment extends EventBusFragment {
             updateTitle();
             updateDays();
             updateLines();
-            updateTimes();
+            updateTime();
         }
+    }
+
+    private void showTimePickerDialog() {
+        //avoid getting results from previous calls
+        EventBus.getDefault().removeStickyEvent(AlertTimeSelected.class);
+        TimePickerFragment newFragment = new TimePickerFragment();
+        Time t = parseTime();
+        if(t != null) {
+            newFragment.setInitialTime(t.getHour(), t.getMinute());
+        }
+        newFragment.show(getSupportFragmentManager());
     }
 }
