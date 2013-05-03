57d59ee2d871a18c26c87c7289f13a96751d69b2
Antony Tran <antonyt@gmail.com>
2013-05-03 17:48:34 +0900

    Use home icon as back/up for edit alert screen.

--- src/com/tfltravelalerts/alerts/EditAlertActivity.java
+++ src/com/tfltravelalerts/alerts/EditAlertActivity.java
@@ -5,6 +5,7 @@ import org.holoeverywhere.app.Fragment;
 
 import android.os.Bundle;
 
+import com.actionbarsherlock.view.MenuItem;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.tfltravelalerts.common.SinglePaneFragmentActivity;
 
@@ -12,6 +13,24 @@ public class EditAlertActivity extends SinglePaneFragmentActivity {
 
     public static final String ALERT_ID_KEY = EditAlertFragment.ALERT_ID_KEY;
 
+    @Override
+    protected void onCreate(Bundle sSavedInstanceState) {
+        super.onCreate(sSavedInstanceState);
+        
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
     @Override
     protected Fragment buildFragment() {
         Fragment fragment = new EditAlertFragment();
