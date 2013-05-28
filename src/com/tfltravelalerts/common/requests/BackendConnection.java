c2a42a46148b7d27c54523f58163d2c715990e56
Pedro Loureiro <github@pedroloureiro.net>
2013-05-28 12:17:16 +0100

    new request type: post

--- src/com/tfltravelalerts/common/requests/BackendConnection.java
+++ src/com/tfltravelalerts/common/requests/BackendConnection.java
@@ -3,21 +3,29 @@ package com.tfltravelalerts.common.requests;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.http.AndroidHttpClient;
 import android.os.Build;
+import android.util.Log;
 
 import com.google.common.base.Joiner;
 import com.tfltravelalerts.TflApplication;
 
 public class BackendConnection {
+    private final static String LOG_TAG = "BackendConnection";
     public final static String prefix = "http://192.168.1.104:8080";
     public final static String userAgent; // sample: android 4 1.5 / client
                                           // 1.0.0
@@ -41,13 +49,8 @@ public class BackendConnection {
         }
     }
 
-    public static BackendConnectionResult get(String path) {
-        if (!path.startsWith("/")) {
-            path = '/'+path;
-        }
-
+    private static BackendConnectionResult doRequest(HttpUriRequest request) {
         AndroidHttpClient httpClient = AndroidHttpClient.newInstance("android");
-        HttpGet request = new HttpGet(prefix + path);
         BackendConnectionResult result;
         try {
             HttpResponse response;
@@ -57,10 +60,30 @@ public class BackendConnection {
             result = new BackendConnectionResult(httpClient, statusLine, content);
         } catch (IOException e) {
             result = new BackendConnectionResult(httpClient, e);
-        } finally {
-           // httpClient.close();
         }
         return result;
+    }
+
+    public static BackendConnectionResult post(String path, NameValuePair... data) {
+        if (!path.startsWith("/")) {
+            path = '/' + path;
+        }
+        HttpPost request = new HttpPost(prefix + path);
+        try {
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(Arrays.asList(data), "UTF-8");
+            request.setEntity(entity);
+        } catch (UnsupportedEncodingException e) {
+            Log.wtf(LOG_TAG, "failed to encode data", e);
+        }
+        return doRequest(request);
+    }
+
+    public static BackendConnectionResult get(String path) {
+        if (!path.startsWith("/")) {
+            path = '/' + path;
+        }
+        HttpGet request = new HttpGet(prefix + path);
+        return doRequest(request);
     }
 
 }
