c1e50692c3fc24156621f45224d0b73c3cab4394
Pedro Loureiro <github@pedroloureiro.net>
2013-06-01 17:26:23 +0100

    removing unneeded code from the line status parser

--- src/com/tfltravelalerts/model/LineStatus.java
+++ src/com/tfltravelalerts/model/LineStatus.java
@@ -7,54 +7,24 @@ import com.tfltravelalerts.R;
  * Possible status for a London Underground line.
  */
 public enum LineStatus {
-    GOOD_SERVICE("Good service", "GS", R.string.line_status_good_service, R.drawable.line_status_good_service),
-    MINOR_DELAYS("Minor delays", "MD", R.string.line_status_minor_delays, R.drawable.line_status_minor_delays),
-    SEVERE_DELAYS("Severe delays", "SD", R.string.line_status_severe_delays, R.drawable.line_status_severe_delays),
-    PART_CLOSURE("Part closure", "PC", R.string.line_status_part_closure, R.drawable.line_status_interruptions),
-    PLANNED_CLOSURE("Planned closure", "CS", R.string.line_status_planned_closure, R.drawable.line_status_interruptions),
-    SUSPENDED("Suspended", "SU", R.string.line_status_suspended, R.drawable.line_status_interruptions),
-    PART_SUSPENDED("Part suspended", "PS", R.string.line_status_part_suspended, R.drawable.line_status_interruptions),
-    REDUCED_SERVICE("Reduced service", "RS", R.string.line_status_reduced_service, R.drawable.line_status_interruptions),
-    BUS_SERVICE("Bus service", "BS", R.string.line_status_bus_service, R.drawable.line_status_interruptions);
+    GOOD_SERVICE(R.string.line_status_good_service, R.drawable.line_status_good_service),
+    MINOR_DELAYS(R.string.line_status_minor_delays, R.drawable.line_status_minor_delays),
+    SEVERE_DELAYS(R.string.line_status_severe_delays, R.drawable.line_status_severe_delays),
+    PART_CLOSURE(R.string.line_status_part_closure, R.drawable.line_status_interruptions),
+    PLANNED_CLOSURE(R.string.line_status_planned_closure, R.drawable.line_status_interruptions),
+    SUSPENDED(R.string.line_status_suspended, R.drawable.line_status_interruptions),
+    PART_SUSPENDED(R.string.line_status_part_suspended, R.drawable.line_status_interruptions),
+    REDUCED_SERVICE(R.string.line_status_reduced_service, R.drawable.line_status_interruptions),
+    BUS_SERVICE(R.string.line_status_bus_service, R.drawable.line_status_interruptions);
 
-    private final String mName;
-    private final String mId;
     private final int mStatusResId;
     private final int mStatusIcon;
 
-    private LineStatus(String name, String id, int statusResId, int statusIcon) {
-        mName = name;
-        mId = id;
+    private LineStatus(int statusResId, int statusIcon) {
         mStatusResId = statusResId;
         mStatusIcon = statusIcon;
     }
 
-    public static LineStatus getLineStatusByName(String name) {
-        for (LineStatus lineStatus : values()) {
-            if (lineStatus.getName().equalsIgnoreCase(name)) {
-                return lineStatus;
-            }
-        }
-        return null;
-        
-    }
-    
-    public String getName() {
-        return mName;
-    }
-    
-    public static LineStatus getLineStatusById(String id) {
-        for (LineStatus lineStatus : values()) {
-            if (lineStatus.getId().equals(id)) {
-                return lineStatus;
-            }
-        }
-        return null;
-    }
-
-    public String getId() {
-        return mId;
-    }
 
     public int getStatusResId() {
         return mStatusResId;
--- src/com/tfltravelalerts/statusviewer/service/LineStatusParser.java
+++ src/com/tfltravelalerts/statusviewer/service/LineStatusParser.java
@@ -4,18 +4,10 @@ package com.tfltravelalerts.statusviewer.service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 import com.google.gson.GsonBuilder;
-import com.tfltravelalerts.model.Line;
-import com.tfltravelalerts.model.LineStatus;
 import com.tfltravelalerts.model.LineStatusServerResponse;
 import com.tfltravelalerts.model.LineStatusUpdate;
 
@@ -32,50 +24,6 @@ public class LineStatusParser {
             LineStatusServerResponse lineStatusUpdates = new GsonBuilder().create().fromJson(response, LineStatusServerResponse.class);
 
             return lineStatusUpdates;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-    
-    public static List<LineStatusUpdate> parseTflResponse(InputStream inputStream) {
-        try {
-            XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
-            parser.setInput(inputStream, null);
-
-            int eventType = parser.getEventType();
-            List<LineStatusUpdate> lineStatusUpdates = new ArrayList<LineStatusUpdate>();
-
-            Line line = null;
-            LineStatus lineStatus = null;
-            String description = null;
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                if (eventType == XmlPullParser.START_TAG) {
-                    String tag = parser.getName();
-                    if (tag.equals("LineStatus")) {
-                        description = parser.getAttributeValue(null, "StatusDetails");
-                    } else if (tag.equals("Line")) {
-                        int lineId = Integer.parseInt(parser.getAttributeValue(null, "ID"));
-                        line = Line.getLineById(lineId);
-                    } else if (tag.equals("Status")) {
-                        String statusId = parser.getAttributeValue(null, "ID");
-                        lineStatus = LineStatus.getLineStatusById(statusId);
-                    }
-                } else if (eventType == XmlPullParser.END_TAG) {
-                    String tag = parser.getName();
-                    if (tag.equals("LineStatus")) {
-                        LineStatusUpdate lineStatusUpdate = new LineStatusUpdate(line,
-                                lineStatus, description);
-                        lineStatusUpdates.add(lineStatusUpdate);
-                    }
-                }
-                eventType = parser.next();
-            }
-
-            return lineStatusUpdates;
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
