a471c2cd28b39c0199acdf0754f66eebacbc7cdf
Pedro L <github@pedroloureiro.net>
2017-10-26 14:09:05 +0100

    add more sample alarms

--- android_app/TfLTravelAlerts/app/src/main/java/com/tfltravelalerts/model/ConfiguredAlarm.kt
+++ android_app/TfLTravelAlerts/app/src/main/java/com/tfltravelalerts/model/ConfiguredAlarm.kt
@@ -48,6 +48,36 @@ data class ConfiguredAlarm(
                         Day.values().toSet(),
                         Line.values().toSet(),
                         true,
+                        false),
+                ConfiguredAlarm(
+                        Time(9,15),
+                        setOf(Day.MONDAY, Day.WEDNESDAY, Day.FRIDAY),
+                        setOf(Line.JUBILEE, Line.METROPOLITAN),
+                        false,
+                        true),
+                ConfiguredAlarm(
+                        Time(19,0),
+                        setOf(Day.MONDAY, Day.WEDNESDAY, Day.FRIDAY),
+                        setOf(Line.JUBILEE, Line.METROPOLITAN),
+                        false,
+                        true),
+                ConfiguredAlarm(
+                        Time(12,30),
+                        setOf(Day.SATURDAY, Day.SUNDAY),
+                        setOf(Line.CENTRAL),
+                        true,
+                        true),
+                ConfiguredAlarm(
+                        Time(12,30),
+                        setOf(Day.SATURDAY, Day.SUNDAY),
+                        Line.values().toSet(),
+                        true,
+                        false),
+                ConfiguredAlarm(
+                        Time(18,30),
+                        Day.values().toSet(),
+                        Line.values().toSet(),
+                        true,
                         false)
         )
     }
